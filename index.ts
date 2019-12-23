55b83f4255c3f6d4d89b89be76b1c91dfb2aab2c
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2019-12-23 12:32:27 -0600

    Add a simple fetcher and transpiler

---
+++ index.ts
@@ -0,0 +1,37 @@
+/** UMD export for 'es-module-shim' module. */
+declare var importShim: {
+  fetch(url: string): Promise<Response>
+}
+
+/** UMD export for TypeScript */
+declare var ts: {
+  fileExtensionIs(path: string, extension: string): boolean
+  transpile(source: string, compilerOptions: object, file: string): string
+}
+
+// Override the fetch call to automattically compile typescript.
+importShim.fetch = async urlString => {
+  const url = new URL(urlString)
+  const response = await fetch(url.toString())
+  if (response.ok && ts.fileExtensionIs(url.pathname, 'ts')) {
+    const source = await response.text()
+    const result = ts.transpile(source, {
+      module: "es2015",
+      target: "es2019",
+      esModuleInterop: true,
+      resolveJsonModule: true,
+      moduleResolution: "node",
+      inlineSourceMap: true,
+      inlineSources: true,
+
+      // Inclime compiler options
+      suppressOutputPathCheck: true,
+      allowNonTsExtensions: true,
+      // Source maps file structure fix.
+      mapRoot: '../../',
+    }, url.pathname)
+      .replace(/.js/g, '.ts')
+    return new Response(new Blob([result], { type: 'application/javascript' }))
+  }
+  return response
+}
